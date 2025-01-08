904f5831618d546f09e597910987086cd88a6476
darrylgunawan0011 <157380754+darrylgunawan0011@users.noreply.github.com>
2025-01-07 20:17:49 -0500

    Add files via upload

--- app.py
+++ app.py
@@ -232,7 +232,7 @@ def download_pdf():
     ])
 
     table.setStyle(style)
-
+ 
     # Build the PDF
     doc.build([table])
 
@@ -245,4 +245,4 @@ def download_pdf():
     return response
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=80)
